e85f70bb95c720e630b1d0b9c0830acc533fe919
Ruben Gonzalez Criado <ruben.criado@yoti.com>
2021-01-29 10:38:42 +0000

    YM-21449: Created initial CocoaPods sample application

---
+++ Samples/CocoaPods/Sample/Sources/Sample/Delegates/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+// Copyright © 2020 Yoti Ltd. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
